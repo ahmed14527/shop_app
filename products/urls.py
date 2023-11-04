ceb3dc5277285a8fd30ffe8a0616b965d89e5087
ahmed14527 <108636071+ahmed14527@users.noreply.github.com>
2023-11-05 00:46:53 +0200

    sss

--- products/urls.py
+++ products/urls.py
@@ -1,15 +1,28 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductTypeViewSet, ProductViewSet, ProductSpecificationViewSet, ProductSpecificationValueViewSet, ProductImageViewSet
+from rest_framework.routers import SimpleRouter
+from . import views
 
-router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'product-types', ProductTypeViewSet)
-router.register(r'products', ProductViewSet)
-router.register(r'product-specifications', ProductSpecificationViewSet)
-router.register(r'product-specification-values', ProductSpecificationValueViewSet)
-router.register(r'product-images', ProductImageViewSet)
+# Create a router and register the viewsets
+router = SimpleRouter()
+router.register('categories', views.CategoryViewSet, basename='category')
+router.register('product-types', views.ProductTypeViewSet, basename='product-type')
+router.register('products', views.ProductViewSet, basename='product')
+router.register('product-specifications', views.ProductSpecificationViewSet, basename='product-specification')
+router.register('product-specification-values', views.ProductSpecificationValueViewSet, basename='product-specification-value')
+router.register('product-images', views.ProductImageViewSet, basename='product-image')
 
+# Define additional URLs
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
+    path('categories/list/', views.CategoryListAPIView.as_view(), name='category-list'),
+    path('product-types/list/', views.ProductTypeListAPIView.as_view(), name='product-type-list'),
+    path('products/list/', views.ProductListAPIView.as_view(), name='product-list'),
+    path('product-specifications/list/', views.ProductSpecificationListAPIView.as_view(), name='product-specification-list'),
+    path('product-specification-values/list/', views.ProductSpecificationValueListAPIView.as_view(), name='product-specification-value-list'),
+    path('product-images/list/', views.ProductImageListAPIView.as_view(), name='product-image-list'),
 ]
+
+# Optional: Add a custom URL pattern for the root API view
+urlpatterns += [
+     path('', views.APIRootView.as_view(), name='api-root'),
+ ]
