c407f3e03b69584a8b4e9c76326d3b2985f009b6
ahmed14527 <108636071+ahmed14527@users.noreply.github.com>
2023-11-13 02:57:49 +0200

    sss

--- testapp/urls.py
+++ testapp/urls.py
@@ -17,30 +17,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from rest_framework.routers import DefaultRouter
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView,
-)
-from rest_framework.documentation import include_docs_urls
+from django.contrib import admin
+from django.urls import path, re_path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-from rest_framework.schemas import get_schema_view
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version="v1",
+        description="API for your project",
+        terms_of_service="https://your-terms-of-service-url.com/",
+        contact=openapi.Contact(email="your-email@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('api/', include('products.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
-    path(
-        'api/docs/',
-        SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs',),
-    path('docs/', include_docs_urls(title='BlogAPI')),
-    path('schema', get_schema_view(
-        title="BlogAPI",
-        description="API for the BlogAPI",
-        version="1.0.0"
-    ), name='openapi-schema'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
 
 ]
