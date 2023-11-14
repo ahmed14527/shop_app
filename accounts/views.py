7b4551f5323b69e787c8ad65b40945086628e0d5
ahmed14527 <108636071+ahmed14527@users.noreply.github.com>
2023-11-14 13:10:17 +0200

    س

--- accounts/views.py
+++ accounts/views.py
@@ -1,34 +1,65 @@
-from rest_framework import generics, permissions
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from knox.models import AuthToken
 from .serializers import UserSerializer, RegisterSerializer
-from django.contrib.auth import login
-from rest_framework import permissions
+from django.contrib.auth import login, logout
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.views import LoginView as KnoxLoginView
 from django.core.exceptions import ObjectDoesNotExist
-from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import logout
-# Register API
+from django.contrib.auth.models import User
 
 
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
+    @swagger_auto_schema(
+        request_body=RegisterSerializer,
+        operation_description="User registration API",
+        responses={
+            200: openapi.Response(
+                description="OK",
+                schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'user': openapi.Schema(
+                            type=openapi.TYPE_OBJECT,
+                            properties={
+                                'id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                                'username': openapi.Schema(type=openapi.TYPE_STRING),
+                                # Add more properties as needed
+                            },
+                        ),
+                        'token': openapi.Schema(type=openapi.TYPE_STRING),
+                    },
+                ),
+            ),
+            400: 'Bad Request',
+        },
+    )
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response({
-        "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
         })
-        
+
+
 class LoginAPI(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
 
+    @swagger_auto_schema(
+        request_body=AuthTokenSerializer,
+        operation_description="User login API",
+        responses={
+            200: 'OK',
+            400: 'Bad Request',
+        },
+    )
     def post(self, request, format=None):
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -36,7 +67,14 @@ class LoginAPI(KnoxLoginView):
         login(request, user)
         return super(LoginAPI, self).post(request, format=None)
 
+
 class LogoutView(APIView):
+    @swagger_auto_schema(
+        operation_description="User logout API",
+        responses={
+            200: openapi.Response(description="OK"),
+        },
+    )
     def post(self, request):
         try:
             request.user.auth_token.delete()
@@ -46,3 +84,31 @@ class LogoutView(APIView):
         logout(request)
 
         return Response({"details": "Successfully logged out"}, status=status.HTTP_200_OK)
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    @swagger_auto_schema(
+        operation_description="Get all users API",
+        responses={
+            200: openapi.Response(
+                description="OK",
+                schema=openapi.Schema(
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Schema(
+                        type=openapi.TYPE_OBJECT,
+                        properties={
+                            'id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                            'username': openapi.Schema(type=openapi.TYPE_STRING),
+                            # Add more properties as needed
+                        },
+                    ),
+                ),
+            ),
+            400: 'Bad Request',
+        },
+    )
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
